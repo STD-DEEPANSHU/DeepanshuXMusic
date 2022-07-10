64a864e589a6cef8a26876a3ad74bc89c58b751d
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-07-10 16:58:45 +0530

    Create __init__.py

---
+++ DeepanshuX/__init__.py
@@ -0,0 +1,41 @@
+from DeepanshuX.core.bot import DeepanshuXBot
+from DeepanshuX.core.dir import dirr
+from DeepanshuX.core.git import git
+from DeepanshuX.core.userbot import Userbot
+from DeepanshuX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = DeepanshuXBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
